43e47846dbd5314b2bf66d7a885329fa0153a769
Julia Alos <juliaalos@gmail.com>
2023-05-13 10:09:17 +0200

    jwt chat

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -8,20 +8,25 @@ use axum::{
     Router,
 };
 use futures::{sink::SinkExt, stream::StreamExt};
+use postgres::{Client, NoTls};
 use serde_derive::{Deserialize, Serialize};
 use std::{net::SocketAddr, sync::Arc};
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, Mutex};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use auth0_jwt::claims::Claims;
+
+#[derive(Clone)]
 struct AppState {
-    tx: broadcast::Sender<Vec<u8>>,
+    tx: broadcast::Sender<(i32, ChatMessage)>,
+    client: Arc<Mutex<Client>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
 struct ChatMessage {
-    chat_id: i32,
     user: String,
     message: String,
+    timestamp: Option<String>,
 }
 
 #[tokio::main]
@@ -31,8 +36,11 @@ async fn main() {
         .init();
 
     let (tx, _rx) = broadcast::channel(100);
+    let client = Arc::new(Mutex::new(
+        Client::connect("host=localhost user=postgres", NoTls).unwrap(),
+    ));
 
-    let app_state = Arc::new(AppState { tx });
+    let app_state = Arc::new(AppState { tx, client });
 
     let app = Router::new()
         .route("/chat/:id", get(websocket_handler))
@@ -50,33 +58,80 @@ async fn websocket_handler(
     ws: WebSocketUpgrade,
     Path(id): Path<i32>,
     State(state): State<Arc<AppState>>,
+    Claims(claims): Claims<ClaimsContent>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| websocket(socket, id, state))
+    ws.on_upgrade(move |socket| websocket(socket, id, state, auth0_jwt::claims::Claims(claims)))
 }
 
-async fn websocket(stream: WebSocket, id: i32, state: Arc<AppState>) {
+async fn websocket(
+    stream: WebSocket,
+    id: i32,
+    state: Arc<AppState>,
+    claims: Claims<ClaimsContent>,
+) {
     let (mut sender, mut receiver) = stream.split();
 
-    // TODO, check user
+    let rows =state
+        .client
+        .blocking_lock()
+        .query("SELECT user_name, message, created_at FROM message,user WHERE team_id = $1 AND user_id = user.id ORDER BY created_at", &[&id]);
+
+    if rows.is_ok() {
+        return;
+    }
+
+    for row in rows.unwrap() {
+        let user: String = row.get(0);
+        let message: String = row.get(1);
+        let timestamp: Option<String> = row.get(2);
+
+        let chat_message = ChatMessage {
+            user,
+            message,
+            timestamp,
+        };
+        if let Ok(msg) = serde_json::to_vec(&chat_message) {
+            if sender.send(Message::Binary(msg)).await.is_err() {
+                return;
+            }
+        }
+    }
 
     let mut rx = state.tx.subscribe();
 
     let mut send_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
-            let message: ChatMessage = serde_json::from_slice(&msg).unwrap_or_default();
-            if message.chat_id == id {
-                if sender.send(Message::Binary(msg)).await.is_err() {
-                    break;
+            if msg.0 == id {
+                if let Ok(message) = serde_json::to_vec(&msg) {
+                    if sender.send(Message::Binary(message)).await.is_err() {
+                        break;
+                    }
                 }
             }
         }
     });
 
     let tx = state.tx.clone();
+    let client = state.client.clone();
 
     let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(Message::Binary(message))) = receiver.next().await {
-            let _ = tx.send(message);
+        while let Some(Ok(Message::Binary(msg))) = receiver.next().await {
+            let message: Result<ChatMessage, serde_json::Error> = serde_json::from_slice(&msg);
+
+            let user_id: i32 = 0; // TODO
+
+            if message.is_ok() {
+                let mut msg = message.unwrap();
+                if let Ok(res) =client
+                    .blocking_lock().query(
+                    "INSERT INTO message (user_id, team_id, message) VALUES ($1, $2, $3) RETURNING created_at",
+                    &[&user_id, &id, &msg.message],
+                ){
+                    let timestrap:String = res.get(0).unwrap().get(0);
+                    msg.timestamp= Some(timestrap);
+                    let _ = tx.send((id, msg));
+                }
+            }
         }
     });
 
@@ -84,4 +139,10 @@ async fn websocket(stream: WebSocket, id: i32, state: Arc<AppState>) {
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
     };
+}
+
+#[derive(Deserialize, Serialize)]
+struct ClaimsContent {
+    pub exp: usize,
+    pub iat: usize,
 }
