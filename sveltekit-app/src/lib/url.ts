c8a0ceb22fe89ed653c47a8f260c392cfd99fde7
Miika Tuominen <miika@beanbakers.fi>
2023-05-13 22:36:09 +0300

    fixx back button

--- sveltekit-app/src/lib/url.ts
+++ sveltekit-app/src/lib/url.ts
@@ -2,3 +2,5 @@ export const getNoteUrl = (courseSlug: string, noteId: string, edit = false) =>
 	`/study/${courseSlug}/notes/${noteId}${edit ? '?edit' : ''}`;
 
 export const getCourseUrl = (courseSlug: string) => `/study/${courseSlug}`;
+
+export const backBehaviour = [[new RegExp('/study/(.*?)/notes/.*'), '/study/$1']] as const;
